fccb077aa2e95c226d21e4495347882ee3eab884
ep3824 <ethan.parent1@gmail.com>
2020-07-08 12:22:29 -0600

    configure pg with Pools and hook up to server routes

---
+++ serverSDC/index2.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const db = require('./queries');
+const app = express();
+const port = 3003;
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(express.static('public'));    <== commented out for now for testing
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+// Future Update: Move routes to their own file, e.g. routes.js
+
+app.get('/', function(req, res) {
+  res.json({ info: 'Node.js, Express, and Postgres API'})
+})
+
+app.get('/users', db.getUsers);
+
+app.post('/', function(req, res) {
+  res.send('POST req to root')
+})
+
+app.put('/', function(req, res) {
+  res.send('PUT req to root')
+})
+
+app.delete('/', function(req, res) {
+  res.send('DELETE req to root')
+})
+
+// Shape of Data:
+/*
+{
+  "product_id": STRING(num)
+  "results": [
+    {
+      "question_id": INT,
+      "question_body": STRING,
+      "question_date": STRING(timestamp),
+      "asker_name": STRING,
+      "question_helpfulness": INT,
+      "reported": INT,
+      "answers": {
+        STRING(num): {
+          "id": INT,
+          "body": STRING,
+          "date": STRING(timestamp),
+          "answerer_name": STRING,
+          "photos": [
+            "URL",
+            "URL"
+          ]
+        },
+        STRING(num): {
+          "id": INT,
+          etc etc etc...
+        }
+      }
+    }
+  ]
+}
+
+** NOTE: Photos can be a empty array, answers can be empty object
+*/
---
+++ serverSDC/queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'ethan_parent',
+  host: 'localhost',
+  database: 'apiexample',
+  password: '',
+  port: 5432,
+})
+
+const getUsers = (req, res) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getUsers,
+}
