791df82f48045cdf48fd0e004c048bc018bd9a05
ep3824 <ethan.parent1@gmail.com>
2020-07-09 15:41:32 -0600

    gather faker scripts needed

--- serverSDC/dataGen.js
+++ serverSDC/dataGen.js
@@ -1,3 +1,4 @@
+var faker = require('faker');
 const pgp = require('pg-promise')();
 const connectionObj = {
   user: 'ethan_parent',
@@ -17,9 +18,23 @@ let answerQueryInsert = `INSERT INTO answers
 (answer_id, body, date, answerer_name, helpfulness, photos, question_id)
 VALUES (1, 'I had no problem when washing in cool water', 'Thu Jul 09 2020 11:58:00', 'ethanFromBoulder', 5,  '{"https://images.unsplash.com/photo-1510551310160-589462daf284?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1649&q=80", "https://images.unsplash.com/photo-1469504512102-900f29606341?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"}', 1);`
 
-db.any(questionQueryInsert)
-  .then(data => {
-    db.any(answerQueryInsert)
-  }).catch(error => {
-    console.log(error);
-  });
+// db.any(questionQueryInsert)
+//   .then(data => {
+//     db.any(answerQueryInsert)
+//   }).catch(error => {
+//     console.log(error);
+//   });
+
+// username
+faker.internet.userName()
+// timestamp
+faker.date.past()
+// random num 1-99
+Math.floor((Math.random() * 99) + 1)
+// title
+faker.lorem.sentence()
+// body
+faker.lorem.paragraph()
+// name
+faker.name.firstName()
+faker.name.lastName()
