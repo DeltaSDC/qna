c9a8d8303df2304480f0ac3cf7868c2c9c8c541c
ep3824 <ethan.parent1@gmail.com>
2020-07-22 18:51:20 -0600

    r

--- serverSDC/queries.js
+++ serverSDC/queries.js
@@ -33,7 +33,7 @@ const getQuestion = (req, res) => {
   }
   if (ethanCache[req.params.id]) {
     let data = ethanCache[req.params.id]
-    res.status(200).json(results.rows)
+    res.status(200).json(data)
   }
 
   pool.query(`SELECT * FROM questions WHERE product_id >= ${rangeMin} AND product_id <= ${rangeMax}`, (error, results) => {
